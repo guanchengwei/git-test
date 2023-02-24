52a3cf3314167779f792bc8df38b108a312c1526
smlz <chengweiguan11@gmail.com>
2023-02-24 19:19:32 +0800

    first commit

---
+++ src/main/java/com/smlz/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.smlz.git;
+
+/**
+ * @BelongsProject: git-test
+ * @Author: smlz
+ * @CreateTime: 2023-02-24  19:16
+ * @Description: TODO
+ * @Version: 1.0
+ * @Since：1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+
+    }
+}
