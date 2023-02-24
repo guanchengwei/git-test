9ee3dea63997dd7b4aa33988b0bcb73c8634ef73
smlz <chengweiguan11@gmail.com>
2023-02-24 19:21:39 +0800

    second commit

--- src/main/java/com/smlz/git/GitTest.java
+++ src/main/java/com/smlz/git/GitTest.java
@@ -11,6 +11,7 @@ package com.smlz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
 
     }
 }
